2e86301be9e6280e2866f7b06f07142aa1badd15
Whispering <ginafro240810@gmail.com>
2026-05-16 18:09:31 +0530

    feat: Added Player Model Rendering with Skin

--- src/main/java/com/jef/justenoughfakepixel/features/profile/viewer/ui/ProfileViewerGUI.java
+++ src/main/java/com/jef/justenoughfakepixel/features/profile/viewer/ui/ProfileViewerGUI.java
@@ -1,12 +1,27 @@
 package com.jef.justenoughfakepixel.features.profile.viewer.ui;
 
 import com.jef.justenoughfakepixel.core.config.gui.GuiTextures;
+import com.jef.justenoughfakepixel.features.capes.Cape;
+import com.jef.justenoughfakepixel.features.capes.CapeManager;
 import com.jef.justenoughfakepixel.features.profile.viewer.PlayerProfile;
 import com.jef.justenoughfakepixel.features.profile.viewer.ProfileViewerAPI;
+import com.jef.justenoughfakepixel.features.profile.viewer.SkinManager;
 import com.jef.justenoughfakepixel.utils.render.NineSliceUtils;
 import com.jef.justenoughfakepixel.utils.render.ResolutionUtils;
+import com.mojang.authlib.GameProfile;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.RenderHelper;
+import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import java.nio.charset.StandardCharsets;
+import java.util.UUID;
 
 public class ProfileViewerGUI extends GuiScreen {
 
@@ -16,6 +31,7 @@ public class ProfileViewerGUI extends GuiScreen {
     // Player Data
     public String activeProfile, username;
     public PlayerProfile playerProfile;
+    public AbstractClientPlayer playerModel;
 
     // State Trackers
     public boolean isFetching = true;
@@ -51,8 +67,8 @@ public class ProfileViewerGUI extends GuiScreen {
     @Override
     public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 
-        int boxW = (int) ResolutionUtils.getXStatic(900);
-        int boxH = (int) ResolutionUtils.getYStatic(600);
+        int boxW = (int) ResolutionUtils.getXStatic(1200);
+        int boxH = (int)(boxW * 9/16.0);
         int boxX = (this.width / 2) - (boxW / 2);
         int boxY = (this.height / 2) - (boxH / 2);
 
@@ -77,12 +93,91 @@ public class ProfileViewerGUI extends GuiScreen {
             drawString(fontRendererObj, text, centerX - (textWidth / 2), centerY - (fontRendererObj.FONT_HEIGHT / 2), 0xFFAAAAAA); // Gray
 
         } else {
-            drawString(fontRendererObj, this.playerProfile.player_name + " (Fetched)", boxX + 5, boxY + 5, 0xFF55FF55); // Green
+            drawString(fontRendererObj, this.playerProfile.player_name + " §8(Fetched)", boxX + 10, boxY + 10, 0xFF55FF55); // Green
+
+            int holeX = boxX + 10;
+            int holeY = boxY + 25;
+            int holeW = boxW / 6;
+            int holeH = (int) (holeW * 1.77777777778);
+            drawRect(holeX, holeY, holeX + holeW, holeY + holeH, 0xFF1A1A1A);
+
+            int scale = 65;
+            int playerX = holeX + (holeW / 2);
+            int playerY = (int) (holeY + (holeH / 2.0) + (scale * 0.9));
+
+            if(playerModel == null){
+                playerModel = new AbstractClientPlayer(mc.theWorld,
+                        new GameProfile(UUID.nameUUIDFromBytes(("OfflinePlayer:" + username).getBytes(StandardCharsets.UTF_8)), username)) {
+
+                    @Override
+                    public ResourceLocation getLocationSkin() {
+                        return SkinManager.getSkin(username);
+                    }
+
+                    @Override
+                    public ResourceLocation getLocationCape() {
+                        Cape cape = CapeManager.getCapeForPlayer(username);
+                        return cape == null ? super.getLocationCape() : cape.resourceLocation;
+                    }
+                };
+
+                playerModel.posX = 9999999.0D;
+                playerModel.posY = 9999999.0D;
+                playerModel.posZ = 9999999.0D;
+            }
+
+            drawEntityOnScreenSmooth(playerX, playerY, scale, mouseX, mouseY, this.playerModel);
 
             // TODO: Draw the rest of fetched profile UI
-
         }
 
         super.drawScreen(mouseX, mouseY, partialTicks);
+    }
+
+    /**
+     * Custom Entity Renderer that calculates exact mouse angles for buttery smooth tracking,
+     * identical to how the Vanilla Inventory GUI does it.
+     */
+    public static void drawEntityOnScreenSmooth(int posX, int posY, int scale, float mouseX, float mouseY, EntityLivingBase ent) {
+        GlStateManager.enableColorMaterial();
+        GlStateManager.pushMatrix();
+        GlStateManager.translate((float)posX, (float)posY, 50.0F);
+        GlStateManager.scale((float)(-scale), (float)scale, (float)scale);
+        GlStateManager.rotate(180.0F, 0.0F, 0.0F, 1.0F);
+
+        float f = (float)posX - mouseX;
+        float f1 = (float)posY - 50.0F - mouseY;
+        GlStateManager.rotate(135.0F, 0.0F, 1.0F, 0.0F);
+        RenderHelper.enableStandardItemLighting();
+        GlStateManager.rotate(-135.0F, 0.0F, 1.0F, 0.0F);
+
+        GlStateManager.rotate(-((float)Math.atan((f / 40.0F))) * 20.0F, 0.0F, 1.0F, 0.0F);
+        ent.renderYawOffset = (float)Math.atan((f / 40.0F)) * 20.0F;
+        ent.rotationYaw = (float)Math.atan((f / 40.0F)) * 40.0F;
+        ent.rotationPitch = -((float)Math.atan((f1 / 40.0F))) * 20.0F;
+        ent.rotationYawHead = ent.rotationYaw;
+        ent.prevRotationYawHead = ent.rotationYaw;
+
+        GlStateManager.translate(0.0F, 0.0F, 0.0F);
+        RenderManager rendermanager = Minecraft.getMinecraft().getRenderManager();
+        rendermanager.setPlayerViewY(180.0F);
+        rendermanager.setRenderShadow(false);
+
+        GlStateManager.clear(GL11.GL_DEPTH_BUFFER_BIT);
+        GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+
+        rendermanager.renderEntityWithPosYaw(ent, 0.0D, 0.0D, 0.0D, 0.0F, 1.0F);
+        rendermanager.setRenderShadow(true);
+        ent.renderYawOffset = 0;
+        ent.rotationYaw = 0;
+        ent.rotationPitch = 0;
+        ent.prevRotationYawHead = 0;
+        ent.rotationYawHead = 0;
+        GlStateManager.popMatrix();
+        RenderHelper.disableStandardItemLighting();
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.setActiveTexture(OpenGlHelper.lightmapTexUnit);
+        GlStateManager.disableTexture2D();
+        GlStateManager.setActiveTexture(OpenGlHelper.defaultTexUnit);
     }
 }
