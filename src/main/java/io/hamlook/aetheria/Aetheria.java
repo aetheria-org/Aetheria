d4af85931d6ac0e88068f71b0c147ccac36d76ef
hamlook <hamlockgit@gmail.com>
2026-05-30 02:43:26 +0500

    vers bump

--- src/main/java/io/hamlook/aetheria/Aetheria.java
+++ src/main/java/io/hamlook/aetheria/Aetheria.java
@@ -28,7 +28,7 @@ public class Aetheria {
 
     public static final String MODID = "aetheria";
     public static final String NAME = "Aetheria";
-    public static final String VERSION = "1.0.2-alpha";
+    public static final String VERSION = "1.0.0-release";
 
     public static ATHRConfig config;
     public static Logger logger;
