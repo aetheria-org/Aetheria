a86451d0d2ca0a796d8089b6d7e7abea232b64f2
hamlook <hamlockgit@gmail.com>
2026-08-15 22:20:32 +0500

    update version to 1.2.0-alpha

--- src/main/java/io/hamlook/aetheria/Aetheria.java
+++ src/main/java/io/hamlook/aetheria/Aetheria.java
@@ -31,7 +31,7 @@ public class Aetheria {
 
     public static final String MODID = "aetheria";
     public static final String NAME = "Aetheria";
-    public static final String VERSION = "1.1.3-alpha";
+    public static final String VERSION = "1.2.0-alpha";
 
     public static ATHRConfig config;
     public static Logger logger;
